0bc945af0e824accf6a4ea7207f36f2e0c67fc13
Josh Talley <jtdev84@gmail.com>
2023-10-04 16:05:54 +0100

    first commit

---
+++ week2.py
@@ -0,0 +1,15 @@
+# firstName = 'Josh'
+# lastName = 'Talley'
+# print('Your first name is ' + firstName + ' and your second name is ' + lastName)
+
+# my_string = 6 * "h" + "ello"
+# print(my_string)
+
+# string = "Have you tried turning it off and on again?"
+# switching = string.replace('turning', 'switching')
+# print(switching)
+
+string = "Turning it on is dangerous."
+uppercase = string[0].upper()
+switching = string.replace('Turning', 'Switching')
+print(switching)
